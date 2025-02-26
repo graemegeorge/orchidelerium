6fcb0b3a431732e0c50a1322affd57de27e6ccfc
Graeme George <graemelgeorge@gmail.com>
2025-02-26 10:08:58 +0000

    fix type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Search } from "./components/search";
 import { SearchingMessage } from "./components/results-client";
 
 interface HomeProps {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 export default async function Home({ searchParams }: HomeProps) {
